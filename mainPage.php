9a5dc5725db7428624e780b1cb17472cf89efa0e
shann <shandra.ilya@gmail.com>
2024-04-25 20:49:21 +0300

    add registration

---
+++ mainPage.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php session_start();?>
+</head>
+<header>
+    <?php include("header.php"); ?>
+</header>
+<body>
+    <?php 
+        if(empty($_SESSION["username"])){
+            echo "Please login";
+        }
+        else if(!empty($_SESSION["username"])){
+            echo "Hello, " . $_SESSION["username"]; 
+         } ?>
+</body>
+</html>
